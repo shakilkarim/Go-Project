3d77dc45c26d75b63b038f9e42eb26407ba5094d
shakilkarim <rezaul921gcr@gmail.com>
2025-10-11 11:53:22 +0600

    variable use

---
+++ Data_Type/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+    var name string
+    var age int
+    var height float64
+
+    name = "John"
+    age = 30
+    height = 5.9
+
+    fmt.Println("Name:", name)
+    fmt.Println("Age:", age)
+    fmt.Println("Height:", height)
+}
