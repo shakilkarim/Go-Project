03df36338e3dc7ecf91d4234c3518d16dff8a438
shakilkarim <rezaul921gcr@gmail.com>
2025-10-12 13:25:19 +0600

    go variable and data type implement

--- Data_Type/main.go
+++ Data_Type/main.go
@@ -3,15 +3,46 @@ package main
 import "fmt"
 
 func main() {
-    var name string
-    var age int
-    var height float64
+    // var name string
+    // var age int
+    // var height float64
 
-    name = "John"
-    age = 30
-    height = 5.9
+    // name = "John"
+    // age = 30
+    // height = 5.9
 
-    fmt.Println("Name:", name)
-    fmt.Println("Age:", age)
-    fmt.Println("Height:", height)
+    // fmt.Println("Name:", name)
+    // fmt.Println("Age:", age)
+    // fmt.Println("Height:", height)
+
+    var a int = 10;
+    var b int = 20;
+
+    var total int = a + b;
+
+    var newPayment float32 = 100000.4477777777;
+    fmt.Println("Payment:", newPayment);
+    var bkashPayment float64 = 70000000000000.7777777777777777777777777;
+
+    var Shakil string = "shakil new";
+
+    fmt.Println("Name: ", Shakil);
+    fmt.Println("Bkash Payment:", bkashPayment);
+    fmt.Println("Total:", total);
+
+    for i:=0; i<5; i++{
+        fmt.Println("Hello World", i);
+    }
+    
+    var isTrue bool = false;
+
+    if isTrue {
+        fmt.Println("This is true");
+    } else {
+        fmt.Println("This is false");
+    }
+
+    for i:= 5; i>0; i--{
+        fmt.Println("Hello Bangladesh", i);
+    }
 }
